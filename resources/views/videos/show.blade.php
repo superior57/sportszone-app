2f446589d8988e4b7dcf4df800b98d1975d49738
Alex <hupi@gmx.de>
2021-04-26 10:40:46 +0200

    Back-Link

--- resources/views/videos/show.blade.php
+++ resources/views/videos/show.blade.php
@@ -1,7 +1,9 @@
 
 @extends('layouts.app')
 @section('content')
+
     <div class="container">
+        <a href="/videos"><h3><- Back to videos</h3></a>
         <div class="row">
 {{--            id is for the API-Path: const API = "https://dev.prosports.zone/api/v1/videos/{$hashid}/annotations";--}}
             <div id="video-wrap" :src="/storage/{{$video->filename}}" :id="{{$video->hashid()}}"></div>
